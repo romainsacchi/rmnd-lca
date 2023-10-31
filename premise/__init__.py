1790ef86fb829b0aa9acebbf362d2b60bf03b07a
romainsacchi <r_s@me.com>
2023-10-31 12:00:45 +0100

    Update wrt master

--- premise/__init__.py
+++ premise/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
+__all__ = ("NewDatabase", "PathwaysDataPackage", "clear_cache", "get_regions_definition")
 __version__ = (1, 8, 1)
 
 
 from .ecoinvent_modification import NewDatabase
+from .pathways import PathwaysDataPackage
 from .utils import clear_cache, get_regions_definition
