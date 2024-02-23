ab7a19ca288176651a8cc5fe7646d0a04f543443
romainsacchi <r_s@me.com>
2024-02-23 09:32:45 +0000

    Black reformating

--- premise/metals.py
+++ premise/metals.py
@@ -490,7 +490,6 @@ class Metals(BaseTransformation):
                 for flow in dataset["additional flow"]:
                     ds["log parameters"]["post-allocation correction"] = flow["amount"]
 
-
                 self.write_log(ds, "updated")
 
     def create_new_mining_activity(
