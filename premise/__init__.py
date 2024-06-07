e079ce3c097a2c047fbc816e5752f5766c927bc0
romainsacchi <r_s@me.com>
2024-06-07 08:52:14 +0000

    Black reformating

--- premise/__init__.py
+++ premise/__init__.py
@@ -10,4 +10,4 @@ __version__ = (2, 1, 1, "dev0")
 
 from premise.new_database import NewDatabase
 from premise.pathways import PathwaysDataPackage
-from premise.utils import clear_cache, get_regions_definition, clear_inventory_cache
+from premise.utils import clear_cache, clear_inventory_cache, get_regions_definition
