5b5f43872fcc96a5ef47e887ccecf40e4e5417e4
romainsacchi <romain@Romains-iMac.psi.ch>
2024-06-25 11:04:52 +0200

    Remove "World" from default value in "except regions"

--- premise/cement.py
+++ premise/cement.py
@@ -417,7 +417,7 @@ class Cement(BaseTransformation):
 
                     scaling_factor = 1 / self.find_iam_efficiency_change(
                         data=self.iam_data.cement_efficiencies,
-                        variable="cement, dry feed rotary kiln",
+                        variable=variable,
                         location=dataset["location"],
                     )
 
@@ -444,7 +444,7 @@ class Cement(BaseTransformation):
 
                         # but if efficient kiln, set the energy input to 3100 kJ/kg clinker
                         if variable.startswith("cement, dry feed rotary kiln, efficient"):
-                            new_energy_input_per_ton_clinker = 3100
+                            new_energy_input_per_ton_clinker = 3000
                             scaling_factor = new_energy_input_per_ton_clinker / current_energy_input_per_ton_clinker
 
                         # rescale fuel consumption and emissions
