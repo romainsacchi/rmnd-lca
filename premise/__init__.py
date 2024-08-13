bb2a3fee28e16e7f78c7a7ff3bcef31cb7dec773
romainsacchi <romain@Romains-iMac.psi.ch>
2024-08-13 12:56:28 +0200

    Align wrt master

--- premise/__init__.py
+++ premise/__init__.py
@@ -5,7 +5,7 @@ __all__ = (
     "clear_inventory_cache",
     "get_regions_definition",
 )
-__version__ = (2, 1, 1, "dev6")
+__version__ = (2, 1, 1, "dev8")
 
 
 from premise.new_database import NewDatabase
